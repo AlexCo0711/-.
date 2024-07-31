0fa070e770bca75dbe61bad6d76ef4e559f3f99e
AlexCo0711 <alexco@list.ru>
2024-07-31 12:30:14 +0300

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,5 +1,5 @@
 home_work = 12
 work_time = 1.5
 qurs = 'Python'
-time_task = work_time/home_work
-print ('Курс:',qurs,','' всего задач:',home_work,','' затрачено часов:',work_time,','' среднее время выполнения:',time_task,'часов.')
+time_for_1_task = work_time/home_work
+print ('Курс:',qurs,','' всего задач:',home_work,','' затрачено часов:',work_time,','' среднее время выполнения:',time_for_1_task,'часов.')
